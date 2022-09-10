8e08529dbb7062a60f8ab93fcceed937c85d9a29
Jitesh Kusalkar <jitesh.kusalkar3@gmail.com>
2022-09-10 15:00:42 +0200

    build: build setup added

---
+++ webpack.prod.config.js
@@ -0,0 +1,111 @@
+const { join, resolve } = require('path');
+const { existsSync } = require('fs');
+const { config } = require('dotenv');
+
+const autoPrefixer = require('autoprefixer');
+const tailwindCSS = require('tailwindcss');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const getEnvironmentKeys = () => {
+  const currentPath = join(__dirname);
+  const basePath = currentPath + '/.env';
+  const envPath = basePath + '.' + process.env.NODE_ENV;
+  const finalPath = existsSync(envPath) ? envPath : basePath;
+  const fileEnv = config({ path: finalPath }).parsed;
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+    return prev;
+  }, {});
+
+  return envKeys;
+};
+
+const configuration = {
+  cache: false,
+  devtool: 'source-map',
+  entry: {
+    app: './src/index.tsx',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        loader: 'html-loader',
+        options: {
+          attrs: ['img:src', 'link:href'],
+        },
+        test: /\.html$/u,
+      },
+      {
+        test: /\.(ts|tsx)$/u,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.json',
+              experimentalFileCaching: true,
+              experimentalWatchApi: true,
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/u,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [tailwindCSS(), autoPrefixer()],
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[hash].[ext]',
+              outputPath: 'assets/images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: 'chunks/[name].js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  performance: {
+    hints: false,
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: true,
+      dry: false,
+      protectWebpackAssets: true,
+      verbose: false,
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      minify: false,
+      template: 'src/index.html',
+    }),
+    new webpack.DefinePlugin(getEnvironmentKeys()),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    mainFields: ['browser', 'module', 'main'],
+  },
+};
+
+module.exports = configuration;
